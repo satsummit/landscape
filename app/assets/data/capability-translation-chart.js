9db17ec34a97d7c4b2f54600c8bb5b18909dc361
Bruno Sánchez-Andrade Nuño <brunosan@gmail.com>
2015-11-08 15:07:10 -0500

    Shorter title

--- app/assets/data/capability-translation-chart.js
+++ app/assets/data/capability-translation-chart.js
@@ -1,6 +1,6 @@
 module.exports = {
   resolution: {
-    display: "[Pan] spatial resolution (m)",
+    display: "Spatial resolution (m)",
     fn: function (x) {
       return x + 'm resolution';
     }
