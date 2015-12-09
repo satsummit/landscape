c22fa97f574ee71c2314c432ed4c02c377fe85c4
smit1678 <nate@developmentseed.org>
2015-12-09 14:48:32 -0500

    remove file listing for debug purposes

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -15,8 +15,5 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; 
   git commit -m "CI deploy to gh-pages"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 else
-  echo "Check for all files created"
-  cd dist
-  ls -la
   echo "Not a publishable branch so we're all done here"
 fi
